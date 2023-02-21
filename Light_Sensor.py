6a1e2b17b96ac7159b052a79f145e8f3e9d4e123
MunawwirSyazilli <125352243+MunawwirSyazilli@users.noreply.github.com>
2023-02-21 11:57:36 +0800

    Create Light_Sensor.py

---
+++ Light_Sensor.py
@@ -0,0 +1,13 @@
+from machine import Pin, ADC
+import time
+
+# digital output light sensor
+digital_sensor_pin = Pin(2, Pin.IN)
+
+while True:
+    # read digital sensor value
+    digital_value = digital_sensor_pin.value()
+    print("Digital value: ", digital_value)
+
+    # wait for 1 second
+    time.sleep(0.5)
