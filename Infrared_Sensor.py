167c1ed7563fa82259f56fd468cf550d5698769e
MunawwirSyazilli <125352243+MunawwirSyazilli@users.noreply.github.com>
2023-02-22 13:35:42 +0800

    Create Infrared_Sensor.py

---
+++ Infrared_Sensor.py
@@ -0,0 +1,13 @@
+from machine import Pin
+import time
+
+# digital input IR sensor
+ir_sensor_pin = Pin(2, Pin.IN)
+
+while True:
+    # read IR sensor value
+    ir_value = ir_sensor_pin.value()
+    print("IR value: ", ir_value)
+
+    # wait for 1 second
+    time.sleep(0.5)
