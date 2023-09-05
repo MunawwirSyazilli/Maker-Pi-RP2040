4fa89f82d4e4b515611df4f49171fc55c4ab5570
MunawwirSyazilli <125352243+MunawwirSyazilli@users.noreply.github.com>
2023-09-05 19:58:05 +0800

    Motor.py

--- Motor1.py
+++ Motor1.py
@@ -1,25 +1,70 @@
-from machine import Pin
+import machine
 import utime
 
-trigger = Pin(3, Pin.OUT)
-echo = Pin(2, Pin.IN)
-
-distance = 0
-def ultrasound():
-    global distance
-    trigger.low()
-    utime.sleep_us(2)
-    trigger.high()
-    utime.sleep_us(5)
-    trigger.low()
-    while echo.value() == 0:
-        signaloff = utime.ticks_us()
-    while echo.value() == 1:
-        signalon = utime.ticks_us()
-    timepassed = signalon - signaloff
-    distance = (timepassed * 0.0343) / 2
+# Setup DC Motor pins
+M1A = machine.PWM(machine.Pin(8))
+M1B = machine.PWM(machine.Pin(9))
+M2A = machine.PWM(machine.Pin(10))
+M2B = machine.PWM(machine.Pin(11))
+M1A.freq(50)
+M1B.freq(50)
+M2A.freq(50)
+M2B.freq(50)
 
 while True:
-    ultrasound()
-    print("Distance = ", distance, "cm")
-    utime.sleep(0.5)
+    print("Forward slow")
+    M1A.duty_u16(0)     # Duty Cycle must be between 0 until 65535
+    M1B.duty_u16(30000)
+    M2A.duty_u16(0)
+    M2B.duty_u16(30000)
+    utime.sleep(1)
+
+    print("Stop")
+    M1A.duty_u16(0)     # Duty Cycle must be between 0 until 65535
+    M1B.duty_u16(0)
+    M2A.duty_u16(0)
+    M2B.duty_u16(0)
+    utime.sleep(1)
+
+    print("Forward")
+    M1A.duty_u16(0)     # Duty Cycle must be between 0 until 65535
+    M1B.duty_u16(65535)
+    M2A.duty_u16(0)
+    M2B.duty_u16(65535)
+    utime.sleep(1)
+
+    print("Stop")
+    M1A.duty_u16(0)     # Duty Cycle must be between 0 until 65535
+    M1B.duty_u16(0)
+    M2A.duty_u16(0)
+    M2B.duty_u16(0)
+    utime.sleep(1)
+
+    print("Backward")
+    M1A.duty_u16(65535)     # Duty Cycle must be between 0 until 65535
+    M1B.duty_u16(0)
+    M2A.duty_u16(65535)
+    M2B.duty_u16(0)
+    utime.sleep(1)
+
+    print("Stop")
+    M1A.duty_u16(0)     # Duty Cycle must be between 0 until 65535
+    M1B.duty_u16(0)
+    M2A.duty_u16(0)
+    M2B.duty_u16(0)
+    utime.sleep(1)
+
+    print("Backward slow")
+    M1A.duty_u16(30000)     # Duty Cycle must be between 0 until 65535
+    M1B.duty_u16(0)
+    M2A.duty_u16(30000)
+    M2B.duty_u16(0)
+    utime.sleep(1)
+
+    print("Brake")
+    M1A.duty_u16(65535)     # Duty Cycle must be between 0 until 65535
+    M1B.duty_u16(65535)
+    M2A.duty_u16(65535)
+    M2B.duty_u16(65535)
+    utime.sleep(1)
+
